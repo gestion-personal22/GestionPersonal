e76765200cc1256259ed1d398f9b138b94c6ca5b
joelruiz2222 <joelybarra222@gmail.com>
2025-10-20 15:09:28 -0300

    Proceso de CRUD_Asistencia Finalizado

--- src/main/java/com/example/GestionPersonal/controller/AsistenciaController.java
+++ src/main/java/com/example/GestionPersonal/controller/AsistenciaController.java
@@ -1,22 +1,62 @@
 package com.example.GestionPersonal.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import com.example.GestionPersonal.entity.Asistencia;
+import com.example.GestionPersonal.service.IAsistenciaService;
 
 @Controller
 @RequestMapping("/asistencia")
 public class AsistenciaController {
 
-	/*@PostMapping
+	@Autowired
+	private IAsistenciaService servAsistencia;
 	
-	@PostMapping
+	@PostMapping("/crear")
+	public String crearAsistencia(Asistencia asistencia) {
+		
+		servAsistencia.crearAsistencia(asistencia);
+		
+		return null;
+	}
 	
-	@PostMapping
+	@PostMapping("/eliminar")
+    public String eliminarAsistencia(@RequestParam("idAsistencia") Long id) {
+		
+		
+		servAsistencia.eliminarAsistencia(id);
+		
+		return null;
+	}
 	
-	@GetMapping
+	@PostMapping("/editar")
+    public String editarAsistencia(@RequestParam("idAsistencia") Long id, 
+    		                       Asistencia asistencia) {
+		
+		servAsistencia.editarAsistencia(id, asistencia);
+		
+		
+		return null;
+	}
 	
-	@GetMapping
-	*/
+	@GetMapping("/buscar")
+    public String buscarAsistencia(@RequestParam("idAsistencia") Long id) {
+		
+		servAsistencia.buscarAsistencia(id);
+		
+		return null;
+	}
+	
+	@GetMapping("/listar")
+    public String listarAsistencias(Model model) {
+		
+		model.addAttribute("Asistencias", servAsistencia.listarAsistencias());
+		
+		return null;
+	}
 }
