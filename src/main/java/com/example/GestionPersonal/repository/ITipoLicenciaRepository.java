62daeeb70e8e8b98155c782053e591e35cda3a24
Clari <yeyoar34@gmail.com>
2025-10-20 20:39:28 -0300

    Proceso de CRUD_TipoLicencia Finalizado

--- src/main/java/com/example/GestionPersonal/repository/ITipoLicenciaRepository.java
+++ src/main/java/com/example/GestionPersonal/repository/ITipoLicenciaRepository.java
@@ -3,9 +3,9 @@ package com.example.GestionPersonal.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.GestionPersonal.entity.Legajo;
+import com.example.GestionPersonal.entity.TipoLicencia;
 
-@Repository 
-public interface ILegajoRepository extends JpaRepository<Legajo, Long> {
+@Repository
+public interface ITipoLicenciaRepository extends JpaRepository<TipoLicencia,Long> {
 
 }
